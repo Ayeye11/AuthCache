99be76e951765dd9ffe0dbec58c203bc8ecd7317
Ayeye11 <ayeye.devp@gmail.com>
2025-03-03 11:05:27 -0300

    feat(infrastructure/database): add Migrator method

--- infrastructure/database/database.go
+++ infrastructure/database/database.go
@@ -1,6 +1,7 @@
 package database
 
 import (
+	"context"
 	"database/sql"
 	"fmt"
 	"time"
@@ -12,8 +13,9 @@ import (
 )
 
 type databaseSQL struct {
-	config config.ConfigSQL
-	db     *gorm.DB
+	engineName string
+	config     config.ConfigSQL
+	db         *gorm.DB
 }
 
 var initDriverFunc = map[string]func(c config.ConfigSQL) gorm.Dialector{
@@ -21,21 +23,50 @@ var initDriverFunc = map[string]func(c config.ConfigSQL) gorm.Dialector{
 	"postgres": func(c config.ConfigSQL) gorm.Dialector { return postgres.Open(c.DSN_postgres()) },
 }
 
-func InitSQL(engineName string, config config.ConfigSQL, attempts int) (*databaseSQL, error) {
+func InitSQL(engineName string, config config.ConfigSQL, attempts ...int) (*databaseSQL, error) {
 	driverFunc, exists := initDriverFunc[engineName]
 	if !exists {
 		return nil, fmt.Errorf("'%s' is not available", engineName)
 	}
 
-	trys := attempts
-	if attempts < 1 || attempts > 10 {
-		trys = 1
+	trys := 1
+	if len(attempts) > 0 && attempts[0] >= 1 && attempts[0] <= 10 {
+		trys = attempts[0]
+	}
+
+	conn := func(ctx context.Context) (*gorm.DB, error) {
+		dbCh := make(chan *gorm.DB, 1)
+		errCh := make(chan error, 1)
+
+		go func() {
+			db, err := gorm.Open(driverFunc(config), &gorm.Config{})
+			if err != nil {
+				errCh <- err
+				return
+			}
+
+			dbCh <- db
+		}()
+
+		select {
+		case err := <-errCh:
+			return nil, err
+
+		case db := <-dbCh:
+			return db, nil
+
+		case <-ctx.Done():
+			return nil, fmt.Errorf("failed to connect to the database: timeout")
+		}
 	}
 
 	for i := 1; i <= trys; i++ {
-		db, err := gorm.Open(driverFunc(config), &gorm.Config{})
+		ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+		defer cancel()
+
+		db, err := conn(ctx)
 		if err == nil {
-			return &databaseSQL{config, db}, nil
+			return &databaseSQL{engineName, config, db}, nil
 		}
 
 		if trys == 1 {
---
+++ infrastructure/database/migrate.go
@@ -0,0 +1,42 @@
+package database
+
+import (
+	"database/sql"
+	"fmt"
+
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database"
+	"github.com/golang-migrate/migrate/v4/database/mysql"
+	"github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+)
+
+const migrationPath = "file://infrastructure/database/migrations"
+
+var migrateDriverFunc = map[string]func(instance *sql.DB) (database.Driver, error){
+	"mysql": func(instance *sql.DB) (database.Driver, error) {
+		return mysql.WithInstance(instance, &mysql.Config{})
+	},
+	"postgres": func(instance *sql.DB) (database.Driver, error) {
+		return postgres.WithInstance(instance, &postgres.Config{})
+	},
+}
+
+func (sql *databaseSQL) GetMigrator() (*migrate.Migrate, error) {
+	driverFunc, exists := migrateDriverFunc[sql.engineName]
+	if !exists {
+		return nil, fmt.Errorf("'%s' is not available", sql.engineName)
+	}
+
+	sqlDB, err := sql.GetInstance()
+	if err != nil {
+		return nil, err
+	}
+
+	driver, err := driverFunc(sqlDB)
+	if err != nil {
+		return nil, err
+	}
+
+	return migrate.NewWithDatabaseInstance(migrationPath, sql.config.DbName, driver)
+}

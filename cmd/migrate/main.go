8f2b92d1b1a59cc603f3ed90490985ae8d2557f7
Ayeye11 <ayeye.devp@gmail.com>
2025-03-03 13:26:51 -0300

    fix(cmd/migrate): Add database migration entrypoint

---
+++ cmd/migrate/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/Ayeye11/se-thr/infrastructure/config"
+	"github.com/Ayeye11/se-thr/infrastructure/database"
+	"github.com/golang-migrate/migrate/v4"
+)
+
+func logf(err error) {
+	if err != nil {
+		log.Fatalf("fatal: %v\n", err)
+	}
+}
+
+func main() {
+	cfg := config.LoadConfig().SQL
+
+	sql, err := database.InitSQL("mysql", cfg)
+	logf(err)
+
+	m, err := sql.GetMigrator()
+	logf(err)
+
+	arg := os.Args[1:]
+
+	if err := runMigration(m, arg...); err != nil {
+		if err != migrate.ErrNoChange {
+			log.Fatalf("fatal: %v\n", err)
+		}
+
+		log.Println("No migrations to apply")
+		return
+	}
+
+	log.Println("migrations applied successfully")
+}
+
+func runMigration(m *migrate.Migrate, arg ...string) error {
+	if len(arg) > 0 && arg[0] != "down" && arg[0] != "up" {
+		return fmt.Errorf("invalid command: use 'up' or 'down'")
+	}
+
+	if len(arg) > 0 && arg[0] == "down" {
+		return m.Down()
+	}
+
+	return m.Up()
+}
